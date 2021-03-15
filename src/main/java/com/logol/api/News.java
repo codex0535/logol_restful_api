27bf74e167cb00f2ea2dcdb3c7c387f9d5ec73ed
pance.s <pance.simorangkir@danamon.co.id>
2021-03-16 02:31:54 +0700

    Created News new object

---
+++ src/main/java/com/logol/api/News.java
@@ -0,0 +1,62 @@
+package com.logol.api;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity 
+public class News {
+	private String id;
+	private String title;
+	private String description;
+	private LocalDate created_date;
+
+	public News() {
+	}
+
+	public News(String id, String title, String description, LocalDate created_date) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.created_date = created_date;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getCreated_date() {
+		return created_date;
+	}
+
+	public void setCreated_date(LocalDate created_date) {
+		this.created_date = created_date;
+	}
+
+}
