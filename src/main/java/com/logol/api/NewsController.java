c2549e77556b9dd5a08ff36f51e20e325068e884
pance.s <pance.simorangkir@danamon.co.id>
2021-03-16 03:14:11 +0700

    Added Controller by Id

--- src/main/java/com/logol/api/NewsController.java
+++ src/main/java/com/logol/api/NewsController.java
@@ -1,9 +1,13 @@
 package com.logol.api;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +20,18 @@ public class NewsController {
 	@GetMapping("/news")
 	public List<News> list() {
 		return service.listAll();
+	}
+	
+	@GetMapping("/news/{id}")
+	public ResponseEntity<News> get(@PathVariable String id) {
+		try {
+			News news = service.get(id);
+			return new ResponseEntity<News>(news, HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<News>(HttpStatus.NOT_FOUND);
+		}
+		
+		
 	}
 	
 }
